1463a28a730023cd57f7f58bc572c0e1d2ad50a8
Chris Dosé <chris@peek.com>
2016-10-17 18:00:28 -0700

    Added ability to add relationships to changesets.

--- lib/lurch/changeset.rb
+++ lib/lurch/changeset.rb
@@ -1,17 +1,24 @@
 module Lurch
   class Changeset
-    attr_reader :id, :type, :attributes
+    attr_reader :id, :type, :attributes, :relationships
 
     def initialize(type, attributes = {})
       is_resource = type.is_a?(Resource)
       @type = is_resource ? type.type : Lurch.normalize_type(type)
       @id = is_resource ? type.id : nil
       @attributes = attributes
+      @relationships = {}
+    end
+
+    def add_related(relationship_key, related_resources)
+      @relationships[relationship_key.to_sym] = related_resources
+      self
     end
 
     def inspect
       attrs = ["id: #{id.inspect}"]
       attrs = attrs.concat attributes.map { |name, value| "#{name}: #{value.inspect}" }
+      attrs = attrs.concat relationships.map { |name, value| "#{name}: #{value.inspect}" }
       inspection = attrs.join(", ")
       "#<#{self.class}[#{Inflecto.classify(type)}] #{inspection}>"
     end
